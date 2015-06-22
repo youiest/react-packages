0eceec0805112ee1f2ea88f9810233da9fa0e9a9
Avital Oliver <avital@thewe.net>
2015-06-22 14:34:53 -0700

    Add all of react-template-helper in one commit
    This will be used for a code review.

---
+++ packages/react-template-helper/test-templates.jsx
@@ -0,0 +1,21 @@
+Template.UsesTextReactComponent.helpers({
+  textComponent() {
+    return TextReactComponent;
+  }
+});
+
+Template.UsesClickableReactComponent.onCreated(function () {
+  this.clicked = false;
+});
+
+Template.UsesClickableReactComponent.helpers({
+  clickableComponent() {
+    return ClickableReactComponent;
+  }
+});
+
+Template.UsesUnmountableComponent.helpers({
+  unmountableComponent() {
+    return UnmountableReactComponent;
+  }
+});
