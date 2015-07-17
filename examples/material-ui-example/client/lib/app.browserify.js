03dce8581cc59318803cd11ddfe05747d84c7e59
wiber <elias@youiest.com>
2015-07-17 11:10:50 +0200

    bad?

--- examples/material-ui-example/client/lib/app.browserify.js
+++ examples/material-ui-example/client/lib/app.browserify.js
@@ -1,5 +1,5 @@
 mui = require('material-ui');
 injectTapEventPlugin = require('react-tap-event-plugin');
-Colors = require('styles/colors');
+Colors = require('colors.js');
 Spacing = require('styles/spacing');
 ColorManipulator = require('utils/color-manipulator');
